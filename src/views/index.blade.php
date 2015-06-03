dd522a08eeb84698596b2401a518208a30a03479
Rizky Leegia <rizky.leegia@wgs.co.id>
2015-06-04 01:05:49 +0700

    Update vendor assets

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -13,54 +13,65 @@ header("Access-Control-Allow-Headers: X-Requested-With");
     <link rel="stylesheet" href="vendor/swaggervel/css/screen.css">
     <link rel="stylesheet" href="vendor/swaggervel/css/screen.css">
 
-    <script src="vendor/swaggervel/lib/shred.bundle.js"></script>
-    <script src="vendor/swaggervel/lib/jquery-1.8.0.min.js"></script>
-    <script src="vendor/swaggervel/lib/jquery.slideto.min.js"></script>
-    <script src="vendor/swaggervel/lib/jquery.wiggle.min.js"></script>
-    <script src="vendor/swaggervel/lib/jquery.ba-bbq.min.js"></script>
-    <script src="vendor/swaggervel/lib/handlebars-1.0.0.js"></script>
-    <script src="vendor/swaggervel/lib/underscore-min.js"></script>
-    <script src="vendor/swaggervel/lib/backbone-min.js"></script>
-    <script src="vendor/swaggervel/lib/swagger.js"></script>
-    <script src="vendor/swaggervel/swagger-ui.js"></script>
-    <script src="vendor/swaggervel/lib/highlight.7.3.pack.js"></script>
+    <script src='vendor/swaggervel/lib/jquery-1.8.0.min.js' type='text/javascript'></script>
+    <script src='vendor/swaggervel/lib/jquery.slideto.min.js' type='text/javascript'></script>
+    <script src='vendor/swaggervel/lib/jquery.wiggle.min.js' type='text/javascript'></script>
+    <script src='vendor/swaggervel/lib/jquery.ba-bbq.min.js' type='text/javascript'></script>
+    <script src='vendor/swaggervel/lib/handlebars-2.0.0.js' type='text/javascript'></script>
+    <script src='vendor/swaggervel/lib/underscore-min.js' type='text/javascript'></script>
+    <script src='vendor/swaggervel/lib/backbone-min.js' type='text/javascript'></script>
+    <script src='vendor/swaggervel/swagger-ui.js' type='text/javascript'></script>
+    <script src='vendor/swaggervel/lib/highlight.7.3.pack.js' type='text/javascript'></script>
+    <script src='vendor/swaggervel/lib/marked.js' type='text/javascript'></script>
+    <script src='vendor/swaggervel/lib/swagger-oauth.js' type='text/javascript'></script>
 
     <!-- enabling this will enable oauth2 implicit scope support -->
     {{--    {{ HTML::script('packages/jlapp/swaggervel/lib/swagger-oauth.js' , array(), $secure); !!}--}}
 
     <script type="text/javascript">
         $(function () {
-            window.swaggerUi = new SwaggerUi({
-                url: "{!! $urlToDocs !!}",
-                dom_id: "swagger-ui-container",
-                supportedSubmitMethods: ['get', 'post', 'put', 'delete'],
-                onComplete: function (swaggerApi, swaggerUi) {
-                    log("Loaded SwaggerUI");
-                    @if(isset($requestHeaders))
-                    @foreach($requestHeaders as $requestKey => $requestValue)
-                    window.authorizations.add("{!!$requestKey!!}", new ApiKeyAuthorization("{!!$requestKey!!}", "{!!$requestValue!!}", "header"));
-                    @endforeach
-                @endif
-                /*if (typeof initOAuth == "function") {
+          var url = "{!! $urlToDocs !!}";
+          if (url && url.length > 1) {
+            url = decodeURIComponent(url[1]);
+          } else {
+            url = "http://petstore.swagger.io/v2/swagger.json";
+          }
+          window.swaggerUi = new SwaggerUi({
+            url: url,
+            dom_id: "swagger-ui-container",
+            supportedSubmitMethods: ['get', 'post', 'put', 'delete', 'patch'],
+            onComplete: function(swaggerApi, swaggerUi){
+              log("Loaded SwaggerUI");
+              @if(isset($requestHeaders))
+                @foreach($requestHeaders as $requestKey => $requestValue)
+                window.authorizations.add("{!!$requestKey!!}", new ApiKeyAuthorization("{!!$requestKey!!}", "{!!$requestValue!!}", "header"));
+                @endforeach
+              @endif
+              
+              // if(typeof initOAuth == "function") {
+              //   initOAuth({
+              //     clientId: "your-client-id",
+              //     realm: "your-realms",
+              //     appName: "your-app-name"
+              //   });
+              // }
 
-                     initOAuth({
-                     clientId: "your-client-id",
-                     realm: "your-realms",
-                     appName: "your-app-name"
-                     });
-                     }*/
-                    $('pre code').each(function (i, e) {
-                        hljs.highlightBlock(e)
-                    });
-                },
-                onFailure: function (data) {
-                    log("Unable to Load SwaggerUI");
-                },
-                docExpansion: "none"
-            });
+              $('pre code').each(function(i, e) {
+                hljs.highlightBlock(e)
+              });
 
-            $('#input_apiKey').change(function () {
-                var key = $('#input_apiKey')[0].value;
+              addApiKeyAuthorization();
+            },
+            onFailure: function(data) {
+              log("Unable to Load SwaggerUI");
+            },
+            docExpansion: "none",
+            apisSorter: "alpha"
+          });
+
+          function addApiKeyAuthorization(){
+            var key = encodeURIComponent($('#input_apiKey')[0].value);
+            if(key && key.trim() != "") {
                 log("key: " + key);
                 if (key && key.trim() != "") {
                     log("added key " + key);
@@ -68,29 +79,37 @@ header("Access-Control-Allow-Headers: X-Requested-With");
                 } else {
                     window.authorizations.remove("key");
                 }
-            })
-            window.swaggerUi.load();
-        });
-    </script>
+            }
+          }
+
+          $('#input_apiKey').change(addApiKeyAuthorization);
+
+          // if you have an apiKey you would like to pre-populate on the page for demonstration purposes...
+          /*
+            var apiKey = "myApiKeyXXXX123456789";
+            $('#input_apiKey').val(apiKey);
+          */
+
+          window.swaggerUi.load();
+
+          function log() {
+            if ('console' in window) {
+              console.log.apply(console, arguments);
+            }
+          }
+      });
+      </script>
 </head>
 <body class="swagger-section">
 <div id='header'>
-    <div class="swagger-ui-wrap">
-        <a id="logo" href="http://swagger.wordnik.com">swagger</a>
-
-        <form id='api_selector'>
-            <div class='input icon-btn'>
-                <image id = "show-pet-store-icon" title = "Show Swagger Petstore Example Apis" src="vendor/swaggervel/images/pet_store_api.png" />
-            </div>
-            <div class='input icon-btn'>
-                <image id = "show-wordnik-dev-icon" title = "Show Wordnik Developer Apis" src="vendor/swaggervel/images/wordnik_api.png" />
-            </div>
-            <div class='input'><input placeholder="http://example.com/api" id="input_baseUrl" name="baseUrl"
-                                      type="text"/></div>
-            <div class='input'><input placeholder="api_key" id="input_apiKey" name="apiKey" type="text"/></div>
-            <div class='input'><a id="explore" href="#">Explore</a></div>
-        </form>
-    </div>
+  <div class="swagger-ui-wrap">
+    <a id="logo" href="http://swagger.io">swagger</a>
+    <form id='api_selector'>
+      <div class='input'><input placeholder="http://example.com/api" id="input_baseUrl" name="baseUrl" type="text"/></div>
+      <div class='input'><input placeholder="api_key" id="input_apiKey" name="apiKey" type="text"/></div>
+      <div class='input'><a id="explore" href="#">Explore</a></div>
+    </form>
+  </div>
 </div>
 
 <div id="message-bar" class="swagger-ui-wrap">&nbsp;</div>
